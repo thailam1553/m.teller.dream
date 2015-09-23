299e033be6a2185cc28e4cf9787ca8faedaed5a1
thailam1553 <lam_nguyen12@yahoo.com>
2015-09-23 21:36:01 +0700

    l
    ll

--- www/js/controllers/story.controller.js
+++ www/js/controllers/story.controller.js
@@ -1,5 +1,5 @@
 angular.module('heaven.controllers', [])
 .controller('storyCtrl', ['$scope', 'storySer', '$stateParams', function($scope, story, $stateParams){
 	$scope.stories = story.all();
-	$scope.detailstory = story.get($stateParams.storyId); 
+	$scope.story = story.get($stateParams.storyId); 
 }])
