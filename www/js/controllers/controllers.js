7fa6d5161d8d974d63b0f3bb7669438de17edef6
hieule1607 <hieule1607@gmail.com>
2015-09-23 17:54:17 +0700

    test

--- www/js/controllers/controllers.js
+++
@@ -1,5 +0,0 @@
-angular.module('heaven.controllers', [])
-
-.controller('storyCtrl', ['$scope', 'storySer', function($scope, story){
-	$scope.stories = story.all();
-}])
--- www/js/controllers/story.controller.js
+++ www/js/controllers/story.controller.js
@@ -1,6 +1,6 @@
 angular.module('heaven.controllers', [])
 .controller('storyCtrl', ['$scope', 'storySer', '$stateParams', function($scope, story, $stateParams){
 	$scope.stories = story.all();
+	$scope.detailstory = story.get($stateParams.storyId);
 
-	$scope.story = story.get($stateParams.storyId);
 }])
